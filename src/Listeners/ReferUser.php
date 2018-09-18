82752bc01d3601ce47e5dd95858d1e1a26e3fa62
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-09-18 16:28:11 -0700

    Referral Id missing is now logged and prevented from causing a stack issue

--- src/Listeners/ReferUser.php
+++ src/Listeners/ReferUser.php
@@ -10,6 +10,11 @@ class ReferUser {
     public function handle(UserReferred $event)
     {
         \Log::debug('ReferUser listener: UserReferred fired!');
+        if (empty($event->referralId)) {
+            \Log::debug('ReferralId not provided so skipping logic'. $event->referralId);
+            return;
+        }
+
         $referralLink = ReferralLink::find($event->referralId);
 
         if (empty($referralLink)) {
